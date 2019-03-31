5b60f4b71103aedeba49740b64244ff5fe60c07d
Юра Синишин <yrambler2001@gmail.com>
2019-03-31 21:25:35 +0300

    implemented PostForm

--- src/pages/PostForm/PostFormContainer.js
+++ src/pages/PostForm/PostFormContainer.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import PostForm from "./PostForm";
+import feedCollection from "../../collections/feed";
+import { Redirect } from "react-router-dom";
+
+
 
 class PostFormContainer extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let post = {
+      author: event.currentTarget.author.value,
+      title: event.currentTarget.title.value,
+      body: event.currentTarget.body.value
+    };
+
+    feedCollection.insert(post);
+    this.props.history.push('/feed');
+  }
   render() {
-    return <PostForm {...this.props} />;
+    return <PostForm handleSubmit={this.handleSubmit} {...this.props} />;
   }
 }
 
