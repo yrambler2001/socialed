7a3879c8dc1bc32376f5e338ff7d2d707cd04506
Юра Синишин <yrambler2001@gmail.com>
2019-04-02 17:04:46 +0300

    Added form validation

--- src/pages/PostForm/PostFormContainer.js
+++ src/pages/PostForm/PostFormContainer.js
@@ -6,6 +6,14 @@ import { Redirect } from "react-router-dom";
 
 
 class PostFormContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      validTitle: undefined,
+      validAuthor: undefined,
+      validBody: undefined
+    }
+  }
   handleSubmit = (event) => {
     event.preventDefault();
     let post = {
@@ -17,8 +25,39 @@ class PostFormContainer extends Component {
     feedCollection.insert(post);
     this.props.history.push('/feed');
   }
+
+
+  onChangeAuthor = (e) => {
+    this.setState({
+      validAuthor: ((t = e.currentTarget.value) => (
+        (t.length > 2) //length must be longer than 2
+        && (t.charAt(0).toUpperCase() === t.charAt(0)) //first char must be capital
+        && (t.length < 30) //length must be shorter than 30
+      ))()
+    })
+  }
+  onChangeBody = (e) => {
+    this.setState({
+      validBody: ((t = e.currentTarget.value) => (
+        (t.length > 5) //length must be longer than 2
+        && (t.charAt(0).toUpperCase() === t.charAt(0)) //first char must be capital
+        && (t.length < 1000) //length must be shorter than 20
+      ))()
+    })
+  }
+  onChangeTitle = (e) => {
+    this.setState({
+      validTitle: ((t = e.currentTarget.value) => (
+        (t.length > 2) //length must be longer than 2
+        && (t.charAt(0).toUpperCase() === t.charAt(0)) //first char must be capital
+        && (t.length < 30) //length must be shorter than 30
+      ))()
+    })
+  }
   render() {
-    return <PostForm handleSubmit={this.handleSubmit} {...this.props} />;
+    return <PostForm onChangeAuthor={this.onChangeAuthor} onChangeTitle={this.onChangeTitle} onChangeBody={this.onChangeBody}
+      handleSubmit={this.handleSubmit} validTitle={this.state.validTitle} validAuthor={this.state.validAuthor}
+      validBody={this.state.validBody} {...this.props} />;
   }
 }
 
